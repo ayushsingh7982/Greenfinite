ac4291f29832d1a3411cd7307ae2966b281c699b
aryanoberoi-fanon <1MS20IS025@msrit.edu>
2025-06-02 13:59:43 +0530

    changed pic and added titles and favicon

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
     >
       <div className="w-screen">
         <Navbar />
-        <img src="asset2.jpg" alt="Logo1" className="w-1/3 mx-auto" />
+        <img src="asset2.png" alt="Logo1" className="w-1/3 mx-auto" />
         <h1 className="font-brand text-4xl text-black">Is Tailwind Config Working?</h1>
 
       </div>
